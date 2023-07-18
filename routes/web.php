c6728d223a5827b5a3ed98439c63781fb24e6ef7
niyaz <hungry_la@mail.ru>
2023-07-18 13:53:53 +0500

    №4.Подключил AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'cabinet',],function (){
+    Route::get('',[AdminController::class,'main'])
+        ->name('cabinet.main');
+
+
 });
